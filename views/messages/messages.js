bb9d68ec5906cc66e31e260ac3f77a2d7b56649b
Benkei Sándor <sanya.benkei@gmail.com>
2021-01-20 09:09:21 +0100

    some style changes

--- views/messages/messages.js
+++ views/messages/messages.js
@@ -1,20 +1,20 @@
 app.controller("messagesCtrl", function($scope) {
     $scope.messages=[
         {id:0, name:'Alessandra Accetta', userName:'Alessa_00345', profilePicture:'./gallery/friendsProfilePictures/girl-01.jpg', msg:[
-            {id:0, author:'he', messageContent:'Hello, Here is a biiiiig surprise!!'},
+            {id:0, author:'', messageContent:'Hello, Here is a biiiiig surprise!!'},
             {id:1, author:'me', messageContent:'Hi, Oh my god :)))))'},
-            {id:2, author:'he', messageContent:'I wish you have a great birthday.. :)'},
+            {id:2, author:'', messageContent:'I wish you have a great birthday.. :)'},
             {id:3, author:'me', messageContent:'Yeah, thank you. It cant be better I think so.. :)'},
-            {id:4, author:'he', messageContent:'I would like to say you toooooo!!'}
+            {id:4, author:'', messageContent:'I would like to say you toooooo!!'}
         ]},
         {id:01, name:'Carmen Bressi', userName:'Carmen_990606', profilePicture:'./gallery/friendsProfilePictures/girl-02.jpg', msg:[
-            {id:0, author:'he', messageContent:'YOOOO, m8'},
+            {id:0, author:'', messageContent:'YOOOO, m8'},
             {id:1, author:'me', messageContent:'Whats up, brother!?'},
-            {id:2, author:'he', messageContent:'How is it going on?!?!?'},
+            {id:2, author:'', messageContent:'How is it going on?!?!?'},
             {id:3, author:'me', messageContent:'Everything fine thanks!!'},
             {id:4, author:'me', messageContent:'And u?'},
-            {id:5, author:'he', messageContent:'YEEEAAH fine..!'},
-            {id:6, author:'he', messageContent:'I have gr8 news for you'}
+            {id:5, author:'', messageContent:'YEEEAAH fine..!'},
+            {id:6, author:'', messageContent:'I have gr8 news for you'}
         ]},
         {id:02, name:'Emilia Cagnina', userName:'EmiliaCagninaaaa', profilePicture:'./gallery/friendsProfilePictures/girl-03.jpg', msg:[]},
         {id:03, name:'Luigi D’Arezzo', userName:'DareZzO_00', profilePicture:'./gallery/friendsProfilePictures/boy-01.jpg', msg:[]},
